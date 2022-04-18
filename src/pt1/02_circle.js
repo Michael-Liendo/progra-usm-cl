16fdb18f35b26e3b323f61121690f01e2b261f80
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-04-18 00:28:41 -0400

    feat: add circle exercises

---
+++ src/pt1/02_circle.js
@@ -0,0 +1,12 @@
+import { prompt } from "../utils/stdin.js";
+
+const radius = await prompt("Put you circle radius: ");
+
+let area = Math.PI * radius ** 2;
+let perimeter = 2 * Math.PI * radius;
+
+console.log(
+  `You circle...\n 
+  Perimeter is: ${perimeter.toFixed(2)} \n 
+  Area is: ${area.toFixed(2)}`
+);
