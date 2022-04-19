dbfec738ccfc3e0da66793ae549fdd38ff2dbfab
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-04-18 23:17:02 -0400

    feat: exercise 03_average

---
+++ src/pt1/03_average.js
@@ -0,0 +1,14 @@
+import { prompt } from "../utils/stdin.js";
+
+console.log("Put your notes.");
+
+let firstNote = parseInt(await prompt("Put your first note: "));
+let secondNote = parseInt(await prompt("Put your second note: "));
+let thirdNote = parseInt(await prompt("Put your third note: "));
+let fourthNote = parseInt(await prompt("Put your fourth note: "));
+
+let totalNotes = firstNote + secondNote + thirdNote + fourthNote;
+
+let notesAverage = totalNotes / 4;
+
+console.log(`Your average notes is ${notesAverage}`);
