60ac1ed040c48914eb29eb31d909346e47f40212
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-04-19 12:19:47 -0400

    feat: exercise 05_inverted_number

---
+++ src/pt1/05_inverted_number.js
@@ -0,0 +1,7 @@
+import { prompt } from "../utils/stdin.js";
+
+let number = await prompt("Put a number: ");
+
+let invertedNumber = number.split("").reverse().join("");
+
+console.log(`You inverted number is: ${invertedNumber}`);
