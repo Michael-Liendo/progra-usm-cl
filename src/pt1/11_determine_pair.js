847d276cd03e99511fafb709081aa735a52e3f24
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-04-30 10:10:33 -0400

    feat: exercise 11 determine pair

---
+++ src/pt1/11_determine_pair.js
@@ -0,0 +1,9 @@
+import { prompt } from "../utils/stdin.js";
+
+let number = parseInt(await prompt("Put you number: "));
+console.log(number % 2);
+if (number % 2 == 0) {
+  console.log("You number is par");
+} else if (number % 2 == 1) {
+  console.log("You number is impar");
+}
