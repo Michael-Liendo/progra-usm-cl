e266081563aca9b0b822d05b52006abac7a4605a
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-04-30 09:40:09 -0400

    feat: what grade do i need
    this exercise is incomplete

---
+++ src/pt1/09_what_grade_do_i_need.js
@@ -0,0 +1,9 @@
+import { prompt } from "../utils/stdin.js";
+
+let lapseOne = parseInt(await prompt("Enter lapse note one: "));
+let lapseTwo = parseInt(await prompt("Enter lapse note two: "));
+let laboratoryNote = parseInt(await prompt("Enter laboratory note: "));
+
+let needNote = (lapseOne + lapseTwo + 0) / (3 + laboratoryNote) - 60;
+
+console.log(`You need ${needNote}`);
