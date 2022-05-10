42eb0e64b749f0faeef5d07aa36628f7ce627a75
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-05-10 16:48:58 -0400

    feat: 14_longest_word

---
+++ src/pt1/14_longest_word.js
@@ -0,0 +1,20 @@
+import { prompt } from "../utils/stdin.js";
+
+let firstWord = await prompt("First word: ");
+let secondWord = await prompt("Second word: ");
+
+if (firstWord.length > secondWord.length) {
+  console.log(
+    `The word ${firstWord} have ${
+      firstWord.length - secondWord.length
+    } more letters than ${secondWord}`
+  );
+} else if (secondWord.length > firstWord.length) {
+  console.log(
+    `The word ${secondWord} have ${
+      secondWord.length - firstWord.length
+    } more letters than ${firstWord}`
+  );
+} else if (firstWord.length === secondWord.length) {
+  console.log("Both words have the same length");
+}
