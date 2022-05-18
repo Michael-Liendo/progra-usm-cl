3bcbd6420f20a7e8cb326e8b3b1768ccc75b359c
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-05-18 19:26:03 -0400

    feat: 15 ordering

--- src/pt1/13_division.js
+++ src/pt1/13_division.js
@@ -1,18 +1,17 @@
-import {prompt} from '../utils/stdin.js'
+import { prompt } from "../utils/stdin.js";
 
-let Dividend = parseInt(await prompt('Put a dividend: '))
-let divider = parseInt(await prompt('Put a divider: '))
+let Dividend = parseInt(await prompt("Put a dividend: "));
+let divider = parseInt(await prompt("Put a divider: "));
 
 let quotient = Dividend / divider;
-let rest = Dividend % divider
+let rest = Dividend % divider;
 
 if (rest !== 0) {
-  console.log('The division is not exact.')
-  console.log('Quotient ' + quotient);
-  console.log('Rest' + rest);
+  console.log("The division is not exact.");
+  console.log("Quotient " + quotient);
+  console.log("Rest" + rest);
 } else {
-  console.log('The division is exact.');
-  console.log('Quotient ' + quotient);
-  console.log('Rest ' + rest);
+  console.log("The division is exact.");
+  console.log("Quotient " + quotient);
+  console.log("Rest " + rest);
 }
-
---
+++ src/pt1/15_ordering.js
@@ -0,0 +1,10 @@
+import { prompt } from "../utils/stdin.js";
+
+let numberOne = parseInt(await prompt("Put a first number: "));
+let numberTwo = parseInt(await prompt("Put a number two: "));
+
+if (numberOne > numberTwo) {
+  console.log(numberTwo, numberOne);
+} else {
+  console.log(numberOne, numberTwo);
+}
